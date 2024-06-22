0eabbfa7ad172253b9438b0c97057232e0e7e343
Vítor <vitor.eliabe0@gmail.com>
2024-06-21 23:29:17 -0300

    Add files via upload

--- source.js
+++ source.js
@@ -1,4 +1,19 @@
 var save;
+var senhacorreta = "SANR"
+var conterror = 0;
 function salva(l){
     save=l
+    cont = 0
+    conterror++
+    if (conterror > 3){
+        return window.location = "zfalha.html"
+    }
+    for(i=0; i < senhacorreta.length; i++){
+        for(j=0; j < save.length; j++){
+            if(save[j] === senhacorreta[i]){
+                cont++
+            }
+        }
+    }
+    return alert(cont + "/" + 5 + "   //   TENTATIVAS RESTANTES:" + [4-conterror]); 
 }
